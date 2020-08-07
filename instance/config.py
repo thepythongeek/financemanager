900d8c51b97f3bfbdebaff6b5a9231ee10b56e5d
thepythongeek <chichakechristian@yahoo.com>
2020-08-07 21:01:25 +0300

    'deployableapp-removedgeneratin-secretkey-every-time-application-loads'

--- instance/config.py
+++ instance/config.py
@@ -1,7 +1,7 @@
 
-import os 
 
-SECRET_KEY = os.urandom(16)
+
+SECRET_KEY = b'\xbc<\xce\x0c\x84\xb5\xa4\xf0\xb3\r@%M\x15\xb5\xa6'
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://qedakhsy6qaq7vq5:m01omxffwz2zyuo1@u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wn1um13pucded4ii'
 SQLALCHEMY_TRACK_MODIFICATIONS = False 
 REMEMBER_COOKIE_DURATION = 31536000
