238ac81aa277132f8d8e21a5afa01c0721f59a14
thepythongeek <chichakechristian@yahoo.com>
2020-08-07 18:07:37 +0300

    'deployableapp-secretkey-now-works'

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,6 @@
 
 import os 
 
-SECRET_KEY = os.urandom()
+SECRET_KEY = os.urandom(16)
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://qedakhsy6qaq7vq5:m01omxffwz2zyuo1@u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wn1um13pucded4ii'
 SQLALCHEMY_TRACK_MODIFICATIONS = False 
